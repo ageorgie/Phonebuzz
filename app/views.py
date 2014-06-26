6688ee23f10a1b65c555399f02ee5bd99ece1bc5
Alexander Georgie <al.georgie@gmail.com>
2014-06-26 15:22:03 -0400

    Merged code into before_request block

--- app/views.py
+++ app/views.py
@@ -4,6 +4,11 @@ from app import validator
 from twilio import twiml
 from flask import request
 
+@app.before_request
+def before_request():
+  if not validator.isValid(request.url, request.headers['X-Twilio-Signature'], request.form):
+    return str("That is invalid")
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -11,9 +16,6 @@ def index():
 
 @app.route('/phase1', methods=['POST'])
 def phase1():
-  if not validator.isValid(request.url, request.headers['X-Twilio-Signature'], request.form):
-    return str("That is invalid")
-  
   resp = twiml.Response()
   with resp.gather(action="/fizzbuzz") as g:
     g.say("Please enter a number followed by the pound symbol")
@@ -22,9 +24,6 @@ def phase1():
 
 @app.route('/fizzbuzz', methods=['POST'])
 def fizzbuzz_req():
-  if not validator.isValid(request.url, request.headers['X-Twilio-Signature'], request.form):
-    return str("That is invalid")
-
   num = request.form['Digits']
   result = fizzbuzz.fizzbuzz(int(num))
   resp = twiml.Response()
