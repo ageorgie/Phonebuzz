988a497b423eb14b0d11154cdf92f2eca1912c36
Alexander Georgie <al.georgie@gmail.com>
2014-06-26 15:02:00 -0400

    Take isValid call out again

--- app/views.py
+++ app/views.py
@@ -14,7 +14,7 @@ def phase1():
   print request.url
   print request.headers['X-Twilio-Signature']
   print request.form
-  return if not validator.isValid(request.url, request.headers['X-Twilio-Signature'], request.form)
+  # return if not validator.isValid(request.url, request.headers['X-Twilio-Signature'], request.form)
   resp = twiml.Response()
   with resp.gather(action="/fizzbuzz") as g:
     g.say("Please enter a number followed by the pound symbol")
