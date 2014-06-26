39acbf83612a37ba29f61fab8425449daf124d52
Alexander Georgie <al.georgie@gmail.com>
2014-06-26 15:00:09 -0400

    Debug - Adding trail statements to phase1 action and removed from validator

--- app/validator.py
+++ app/validator.py
@@ -1,21 +1,12 @@
-print "START OF VALIDATOR"
 from twilio.util import RequestValidator
 import os
 
-print "BEFORE AUTH_TOKEN"
 AUTH_TOKEN = ''
 
 if AUTH_TOKEN == '':
   AUTH_TOKEN = os.environ['TWILIO_TOKEN']
 
-print "AFTER AUTH_TOKEN"
-
 validator = RequestValidator(AUTH_TOKEN)
 
-print "AFTER VALIDATOR INITIALIZATION"
-print validator
 def isValid(url, signature, postVars = {}):
-  print "I AM HERE"
   return validator.validate(url, postVars, signature)
-
-print "AFTER isValid DECLARATION"
--- app/views.py
+++ app/views.py
@@ -11,6 +11,9 @@ def index():
 
 @app.route('/phase1', methods=['POST'])
 def phase1():
+  print request.url
+  print request.headers['X-Twilio-Signature']
+  print request.form
   return if not validator.isValid(request.url, request.headers['X-Twilio-Signature'], request.form)
   resp = twiml.Response()
   with resp.gather(action="/fizzbuzz") as g:
