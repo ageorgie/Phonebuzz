35e4c9aca761f7424356e3cceb3b9b3c5b02a840
Alexander Georgie <al.georgie@gmail.com>
2014-06-26 09:41:58 -0400

    Add in the input of a number and output fizzbuzz

--- app/views.py
+++ app/views.py
@@ -9,7 +9,19 @@ def index():
 @app.route('/phase1', methods=['GET', 'POST'])
 def phase1():
   resp = twiml.Response()
-  resp.say('Hey welcome to PhoneBuzz')
+  with resp.gather(action="/fizzbuzz") as g:
+    g.say("Please enter a number followed by the pound symbol")
+
+  return str(resp)
+
+@app.route('/fizzbuzz', methods=['GET', 'POST'])
+def fizzbuzz():
+  num = @app.request.args.get('Digits')
+  
+  result = fizzbuzz(num)
+  
+  resp = twiml.Response()
+  resp.say(result)
 
   return str(resp)
   
