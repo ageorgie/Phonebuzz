0945357bb4e5d34a0f3b2d16865186661cb6ea83
Alexander Georgie <al.georgie@gmail.com>
2014-06-26 01:00:11 -0400

    Try taking out twilio client

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 from app import views
-from app import twilio_client
+# from app import twilio_client
 
-twilio_client = twilio_client.client
+# twilio_client = twilio_client.client
