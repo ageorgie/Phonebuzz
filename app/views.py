32882de60cf917d22e391d1d9667a5c67b6926c9
Alexander Georgie <al.georgie@gmail.com>
2014-06-26 17:12:58 -0400

    Remove extra slash in url

--- app/views.py
+++ app/views.py
@@ -38,5 +38,5 @@ def fizzbuzz_req():
 def start_outgoing_call():
   num = request.form['phone']
   print request.url_root+"/phase1"
-  twilio_client.client.calls.create(to=num, from_="4378000684", url=request.url_root+"/phase1")
+  twilio_client.client.calls.create(to=num, from_="4378000684", url=request.url_root+"phase1")
   return "The call should start momentarily"
