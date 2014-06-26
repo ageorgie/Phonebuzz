19b7becaabfc3c0de5c8195c7e603c28fb71fa17
Alexander Georgie <al.georgie@gmail.com>
2014-06-26 14:29:53 -0400

    Debug - put trail comments

--- app/validator.py
+++ app/validator.py
@@ -1,13 +1,17 @@
+print "START OF VALIDATOR"
 from twilio.util import RequestValidator
 import os
 
+print "BEFORE AUTH_TOKEN"
 AUTH_TOKEN = ''
 
 if AUTH_TOKEN == '':
-	AUTH_TOKEN = os.environ['TWILIO_TOKEN']
+  AUTH_TOKEN = os.environ['TWILIO_TOKEN']
+
+print "AFTER AUTH_TOKEN"
 
 validator = RequestValidator(AUTH_TOKEN)
 
 def isValid(url, signature, postVars = {}):
-	print "I AM HERE"
-	return validator.validate(url, postVars, signature)
+  print "I AM HERE"
+  return validator.validate(url, postVars, signature)
