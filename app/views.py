2e8580b195c1c9e14468af865afda22207d49fa0
Alexander Georgie <al.georgie@gmail.com>
2014-06-26 13:16:04 -0400

    Debug - print headers

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from app import app
 from app import fizzbuzz
+# from app import validator
 from twilio import twiml
 from flask import request
 
@@ -8,16 +9,18 @@ from flask import request
 def index():
   return "Welcome to the PhoneBuzz Test!"
 
-@app.route('/phase1', methods=['GET', 'POST'])
+@app.route('/phase1', methods=['POST'])
 def phase1():
+  # return if not validator.isValid(request.url, request.headers[''])
   resp = twiml.Response()
   with resp.gather(action="/fizzbuzz") as g:
     g.say("Please enter a number followed by the pound symbol")
 
   return str(resp)
 
-@app.route('/fizzbuzz', methods=['GET', 'POST'])
+@app.route('/fizzbuzz', methods=['POST'])
 def fizzbuzz_req():
+  print request.headers
   num = request.form['Digits']
   result = fizzbuzz.fizzbuzz(int(num))
   resp = twiml.Response()
