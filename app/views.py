5ec5863424d5f9f9d724435b605419e579fbe232
Alexander Georgie <al.georgie@gmail.com>
2014-06-26 14:26:03 -0400

    Try taking out validator

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from app import fizzbuzz
-from app import validator
+# from app import validator
 from twilio import twiml
 from flask import request
 
@@ -11,7 +11,7 @@ def index():
 
 @app.route('/phase1', methods=['POST'])
 def phase1():
-  return if not validator.isValid(request.url, request.headers['X-Twilio-Signature'], request.form)
+  # return if not validator.isValid(request.url, request.headers['X-Twilio-Signature'], request.form)
   resp = twiml.Response()
   with resp.gather(action="/fizzbuzz") as g:
     g.say("Please enter a number followed by the pound symbol")
